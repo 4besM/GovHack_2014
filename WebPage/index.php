05dbda4a05338cc5262160a5a5eebb6dc50bdb0e
Pydronia <jackscmail@gmail.com>
2014-07-13 11:04:28 +1000

    Adds Webpage

---
+++ WebPage/index.php
@@ -0,0 +1,7 @@
+<?php
+	function redirect($url, $permanent = false){
+    	if (headers_sent() === false){
+    		header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);}
+    		exit();}
+	redirect('act_art', false);
+?>
